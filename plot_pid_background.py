3e6c269452ac456cd56c45c5cb0ae2c70a161233
Brooke Elyse Russell Morrison <brussell@login23.chn>
2023-05-17 10:28:09 -0700

    beam background plotting scripts

--- plot_pid_background.py
+++ plot_pid_background.py
@@ -9,30 +9,34 @@ import auxiliary
 import glob
 import json
 from mpl_toolkits.axes_grid1.inset_locator import (inset_axes, InsetPosition, mark_inset)
-# threshold backgrounds: ==> only addressing charged pions for now...!!!
-# (1) any charged pions in event are sufficiently short such that they are undetectable --> less than 3 cm (6-7 pixels)
-# (2) one pi0 gamma escapes FV
-# (3) pi0 gammas inbalance: gamma1>>gamma2
-# (4) pi0 gammas are colinear
 
 
 def charged_pion_threshold(d, threshold):
-    cp_length={} # longest charged pion per neutrino interaction
+    cp_count={} # charged pion count above tracking threshold per neutrino interaction
     for key in d.keys():
         if d[key]['pdg']==abs(211):
             contained_length=d[key]['contained_length']
-            if contained_length>0.:
+            if contained_length>threshold:
                 spill_id = key.split('-')[0]
                 vertex_id = key.split('-')[1]
                 temp=(spill_id, vertex_id)
-                if temp not in cp_length: cp_length[temp]=0.
-                if contained_length>cp_length[temp]:
-                    cp_length[temp]=contained_length
+                if temp not in cp_count: cp_count[temp]=0.
+                cp_count[temp]+=1
+
+    cp_length={} # single track MIP length per neutrino interaction
+    for key in d.keys():
+        spill_id = key.split('-')[0]
+        vertex_id = key.split('-')[1]
+        temp=(spill_id, vertex_id)
+        if temp in cp_count.keys():
+            if cp_count[temp]!=1: continue
+            contained_length=d[key]['contained_length']
+            cp_length[temp]=contained_length            
 
     count_bgd=0
     for key in cp_length.keys():
-        if cp_length[key]<=threshold: count_bgd+=1
-    print(count_bgd,' irreducible CC backgrounds with ',threshold,' cm tracking threshold')
+        if cp_length[key]>threshold: count_bgd+=1
+    print(count_bgd,' irreducible NC backgrounds with ',threshold,' cm tracking threshold')
 
     lbins=np.linspace(0,200,51); sbins=np.linspace(0,10,51)
     fig, ax = plt.subplots(figsize=(8,8))
@@ -44,35 +48,27 @@ def charged_pion_threshold(d, threshold):
     axtwin.hist([cp_length[key] for key in cp_length.keys()], bins=lbins, histtype='step',\
                 cumulative=True, density=True, linestyle='solid', color='r')
 
-    ax1 = plt.axes([0,0,1,1])
-    ip =InsetPosition(ax, [0.4, 0.3, 0.5, 0.5])
-    ax1.set_axes_locator(ip)
-    mark_inset(ax, ax1, loc1=2, loc2=4, fc='none', ec='0.5')
-    
-    ax1.hist([cp_length[key] for key in cp_length.keys()], bins=sbins, histtype='step', color='b')
-    ax1.set_xlim(0,10)
-    ax1.axvline(x=threshold, linestyle='dashed', color='orange', label='tracking threshold')
-
     axtwin.set_ylabel('Cumulative Probability')
     ax.legend(loc='upper right')
     ax.set_xlabel('Length [cm]')
     ax.set_ylabel(r'$\nu$ Interactions')
     ax.set_title('Maximum Charged Pion Contained Length'+'\n'+\
-                 r'Per CC $\nu$ Interaction')
-    plt.savefig('irreducible_cc_beam_bgd.png')
+                 r'Per $\nu$ NC Interaction')
+    plt.savefig('irreducible_nc_beam_bgd.png')
 
+    
 
-def main(cc_json_file, tracking_threshold):
-    f = open(cc_json_file)
-    cc_pion_dict=json.load(f)
-    charged_pion_threshold(cc_pion_dict, tracking_threshold)
+def main(nc_json_file, tracking_threshold):
+    f = open(nc_json_file)
+    nc_pion_dict=json.load(f)
+    charged_pion_threshold(nc_pion_dict, tracking_threshold)
     
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-cc', '--cc_json_file', default=None, required=False, type=str, \
-                        help='''string corresponding to the path of the CC pion backgrounds JSON file''')
+    parser.add_argument('-nc', '--nc_json_file', default=None, required=False, type=str, \
+                        help='''string corresponding to the path of the NC pion backgrounds JSON file''')
     parser.add_argument('-t', '--tracking_threshold', default=3., required=False, type=float, \
                         help='''Tracking threshold in track length [cm]''')
     args = parser.parse_args()
