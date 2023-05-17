b91500c36270d92d82f3506db975bbb2d63137b6
Brooke Elyse Russell Morrison <brussell@login23.chn>
2023-05-17 14:54:06 -0700

    address beam background comments from meeting

--- plot_pid_background.py
+++ plot_pid_background.py
@@ -11,7 +11,13 @@ import json
 from mpl_toolkits.axes_grid1.inset_locator import (inset_axes, InsetPosition, mark_inset)
 
 
-def charged_pion_threshold(d, threshold):
+def files_processed(processed_files, total_files=1023, \
+                    production_pot=1e19, target_pot=2.5e19):
+    return target_pot/((processed_files*production_pot)/total_files)
+
+
+
+def charged_pion_threshold(d, threshold, scale_factor):
     cp_count={} # charged pion count above tracking threshold per neutrino interaction
     for key in d.keys():
         if d[key]['pdg']==abs(211):
@@ -38,30 +44,35 @@ def charged_pion_threshold(d, threshold):
         if cp_length[key]>threshold: count_bgd+=1
     print(count_bgd,' irreducible NC backgrounds with ',threshold,' cm tracking threshold')
 
-    lbins=np.linspace(0,200,51); sbins=np.linspace(0,10,51)
-    fig, ax = plt.subplots(figsize=(8,8))
+    bins=np.linspace(0,200,41)
+    fig, ax = plt.subplots(figsize=(6,6))
 
-    ax.hist([cp_length[key] for key in cp_length.keys()], bins=lbins, histtype='step', color='b')
-    ax.axvline(x=threshold, linestyle='dashed', color='orange', label='tracking threshold')
+    meson_length=[cp_length[key] for key in cp_length.keys()]
+    weight=[scale_factor]*len(meson_length)
+    ax.hist(meson_length, bins=bins, weights=weight, histtype='step', color='b')
+    ax.axvline(x=threshold, linestyle='dashed', color='orange', label='3 cm tracking threshold')
     ax.set_xlim(0,200)
+    ax.legend(loc='lower right')
+    ax.set_xlabel(r'Maximum 2x2-contained $\pi^{\pm}$ Track Length [cm]')
+    ax.set_ylabel(r'$\nu$ Interactions / 5 cm')
+    
     axtwin = ax.twinx()
-    axtwin.hist([cp_length[key] for key in cp_length.keys()], bins=lbins, histtype='step',\
+    axtwin.hist(meson_length, bins=bins, weights=weight, histtype='step',\
                 cumulative=True, density=True, linestyle='solid', color='r')
-
     axtwin.set_ylabel('Cumulative Probability')
-    ax.legend(loc='upper right')
-    ax.set_xlabel('Length [cm]')
-    ax.set_ylabel(r'$\nu$ Interactions')
-    ax.set_title('Maximum Charged Pion Contained Length'+'\n'+\
-                 r'Per $\nu$ NC Interaction')
+
+    ax.text(115,2250,r'NuMI ME RHC 2.5$\times$10$^{19}$ POT')
+    ax.text(0,2250,r'$\nu$NC')
+
     plt.savefig('irreducible_nc_beam_bgd.png')
 
     
 
-def main(nc_json_file, tracking_threshold):
+def main(nc_json_file, tracking_threshold, n_files_processed):
     f = open(nc_json_file)
     nc_pion_dict=json.load(f)
-    charged_pion_threshold(nc_pion_dict, tracking_threshold)
+    scale_factor = files_processed(n_files_processed)
+    charged_pion_threshold(nc_pion_dict, tracking_threshold, scale_factor)
     
 
 
@@ -71,5 +82,7 @@ if __name__=='__main__':
                         help='''string corresponding to the path of the NC pion backgrounds JSON file''')
     parser.add_argument('-t', '--tracking_threshold', default=3., required=False, type=float, \
                         help='''Tracking threshold in track length [cm]''')
+    parser.add_argument('-n', '--n_files_processed', default=1, required=True, type=int, \
+                        help='''File count of number of files processed in production sample''')
     args = parser.parse_args()
     main(**vars(args))
--- plot_threshold_background.py
+++ plot_threshold_background.py
@@ -15,8 +15,13 @@ from mpl_toolkits.axes_grid1.inset_locator import (inset_axes, InsetPosition, ma
 # (3) pi0 gammas inbalance: gamma1>>gamma2
 # (4) pi0 gammas are colinear
 
+def files_processed(processed_files, total_files=1023, \
+                    production_pot=1e19, target_pot=2.5e19):
+    return target_pot/((processed_files*production_pot)/total_files)
+    
 
-def charged_pion_threshold(d, threshold):
+
+def charged_pion_threshold(d, threshold, scale_factor):
     cp_length={} # longest charged pion per neutrino interaction
     for key in d.keys():
         if d[key]['pdg']==abs(211):
@@ -34,38 +39,45 @@ def charged_pion_threshold(d, threshold):
         if cp_length[key]<=threshold: count_bgd+=1
     print(count_bgd,' irreducible CC backgrounds with ',threshold,' cm tracking threshold')
 
-    lbins=np.linspace(0,200,51); sbins=np.linspace(0,10,51)
-    fig, ax = plt.subplots(figsize=(8,8))
+    lbins=np.linspace(0,200,41); sbins=np.linspace(0,10,11)
+    fig, ax = plt.subplots(figsize=(6,6))
 
-    ax.hist([cp_length[key] for key in cp_length.keys()], bins=lbins, histtype='step', color='b')
-    ax.axvline(x=threshold, linestyle='dashed', color='orange', label='tracking threshold')
+    meson_length=[cp_length[key] for key in cp_length.keys()]
+    weight=[scale_factor]*len(meson_length)
+
+    ax.hist(meson_length, bins=lbins, weights=weight, histtype='step', color='b')
+    ax.axvline(x=threshold, linestyle='dashed', color='orange', label='3 cm tracking threshold')
     ax.set_xlim(0,200)
+    ax.legend(loc='lower right')
+    ax.set_xlabel(r'Maximum 2x2-contained $\pi^{\pm}$ Track Length [cm]')
+    ax.set_ylabel(r'$\nu$ Interactions / 5 cm')
+    
     axtwin = ax.twinx()
     axtwin.hist([cp_length[key] for key in cp_length.keys()], bins=lbins, histtype='step',\
                 cumulative=True, density=True, linestyle='solid', color='r')
-
+    axtwin.set_ylabel('Cumulative Probability')
+    
     ax1 = plt.axes([0,0,1,1])
     ip =InsetPosition(ax, [0.4, 0.3, 0.5, 0.5])
     ax1.set_axes_locator(ip)
     mark_inset(ax, ax1, loc1=2, loc2=4, fc='none', ec='0.5')
     
-    ax1.hist([cp_length[key] for key in cp_length.keys()], bins=sbins, histtype='step', color='b')
+    ax1.hist(meson_length, bins=sbins, weights=weight, histtype='step', color='b')
     ax1.set_xlim(0,10)
     ax1.axvline(x=threshold, linestyle='dashed', color='orange', label='tracking threshold')
+#    ax1.set_xlabel(r'$\pi^{\pm}$ Length [cm]')
+#    ax1.set_ylabel(r'$\nu$ Interactions')
 
-    axtwin.set_ylabel('Cumulative Probability')
-    ax.legend(loc='upper right')
-    ax.set_xlabel('Length [cm]')
-    ax.set_ylabel(r'$\nu$ Interactions')
-    ax.set_title('Maximum Charged Pion Contained Length'+'\n'+\
-                 r'Per CC $\nu$ Interaction')
+    ax.text(115,2420,r'NuMI ME RHC 2.5$\times$10$^{19}$ POT')
+    ax.text(0,2420,r'$\nu$CC')
     plt.savefig('irreducible_cc_beam_bgd.png')
 
 
-def main(cc_json_file, tracking_threshold):
+def main(cc_json_file, tracking_threshold, n_files_processed):
     f = open(cc_json_file)
     cc_pion_dict=json.load(f)
-    charged_pion_threshold(cc_pion_dict, tracking_threshold)
+    scale_factor = files_processed(n_files_processed)
+    charged_pion_threshold(cc_pion_dict, tracking_threshold, scale_factor)
     
 
 
@@ -75,5 +87,7 @@ if __name__=='__main__':
                         help='''string corresponding to the path of the CC pion backgrounds JSON file''')
     parser.add_argument('-t', '--tracking_threshold', default=3., required=False, type=float, \
                         help='''Tracking threshold in track length [cm]''')
+    parser.add_argument('-n', '--n_files_processed', default=1, required=True, type=int, \
+                        help='''File count of number of files processed in production sample''')
     args = parser.parse_args()
     main(**vars(args))
